2347222c7e601f41fd00a45e8799edee149f3d6c
chsieh099 <9427270+chsieh099@users.noreply.github.com>
2025-06-01 19:33:48 -0400

    Show limit and add templates for history and category buttons

--- js/accumulator.js
+++ js/accumulator.js
@@ -1,21 +1,25 @@
-let current = 0;
 let limit = 10;
+let current = 0;
 
 let limitInputElement;
-
-
+let currentValueElement;
 
 function accumulate() {
 	const page = document.getElementById('page');
 	if (current + 1 > limit) return;
 	current += 1;
-	updateBackground();
+	_updateCurrent();
+	_updateBackground();
 }
 
-function updateBackground() {
+function _updateBackground() {
 	const percentage = (current/limit) * 100 + "%";
 	const gradient = 'linear-gradient(to top, lightblue, lightblue ' + percentage + ', transparent ' + percentage + ', transparent)';
     page.style.backgroundImage = gradient;
+}
+
+function _updateCurrent() {
+	currentValueElement.textContent = current.toString();
 }
 
 function updateLimit() {
@@ -23,11 +27,21 @@ function updateLimit() {
 	if (newLimit != limit) {
 		current = 0;
 		limit = newLimit;
-		updateBackground();
+		_updateCurrent();
+		_updateBackground();
 	}
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  limitInputElement = document.getElementById('limit-input');
-  limitInputElement.value = limit.toString();
+	// Initialize values.
+	limitInputElement = document.getElementById('limit-input');
+	limitInputElement.value = limit.toString();
+
+	currentValueElement = document.getElementById('current');
+	currentValueElement.textContent = current.toString();
+
+	historyButtonElement = document.getElementById('history');
+	historyButtonElement.addEventListener('dblclick', (e) =>
+	  console.log('Double tap detected!')
+	);
 });
